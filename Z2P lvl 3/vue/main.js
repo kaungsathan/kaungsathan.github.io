47ad5e413587c207040647d8cc7cb508d904e671
kaungsathan <kaungsathun19918@gmail.com>
2022-08-04 17:32:42 +0630

    bmi calculator

---
+++ Z2P lvl 3/vue/main.js
@@ -0,0 +1,47 @@
+// const { createApp } = Vue
+
+const ap = Vue.createApp({
+    data() {
+        return {
+            message: '<p style="color: red;">Hello World</p>',
+            name: 'kaungsat',
+            age: 21,
+            photo: '../bootstrap/sneaker/assets/images/favicon.png',
+            count: 1,
+            username: '',
+            color: ['red', 'green', 'blue'],
+            student: [
+                { name: 'mgmg', age: 20, gender: 'male' },
+                { name: 'mama', age: 20, gender: 'female' }
+            ],
+
+        }
+    },
+    methods: {
+        openAlert(msg) {
+            alert(this.name);
+            alert(msg);
+        }
+    },
+});
+
+const app2 = Vue.createApp({
+    data() {
+        return {
+            feet: '',
+            inch: '',
+            pound: '',
+            totalInch: '',
+            semiBmi: '',
+            finalBmi: '',
+        }
+    },
+    
+    methods: {
+        calculate() {
+            this.totalInch = (this.feet * 12) + (this.inch);
+            this.semiBmi = this.pound * 703;
+            this.finalBmi = this.semiBmi / (this.totalInch * this.totalInch);
+        }
+    },
+});
